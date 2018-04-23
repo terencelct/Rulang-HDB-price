808ab95e7ab2deb72b0547e34c6f2b333d113685
lct <ctli@gmail.com>
2018-04-23 09:19:41 +0800

    second

--- server.R
+++ server.R
@@ -1,22 +1,27 @@
 library(shiny)
+library(rsconnect)
+
 
 shinyServer(function(input,output){
-  
-  
-  
+
+  Rdata2 <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQDJVOSM1jFw769RZa0jkB4ovuJFnuaeFePoTzghfOrsrYgm3g3r_YcDaQjYcyxFFxbdMnU6cfyjOHP/pub?gid=1746464075&single=true&output=csv")
   output$density<-renderPlot({
     
     newd <- subset(Rdata2, year==input$year)
     newd2 <-subset(newd,distance<=input$distance)
-    x <- newd2$realprice
+    newd3 <- as.data.frame(newd2)
+    colm <- as.numeric(input$var)
+    x <- newd3[,colm]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
     hist(x)
-    h<-hist(x, breaks=bins, col="red", xlab="Housing price",main=NA) 
+    h<-hist(x, breaks=bins, col="green", xlab="Housing price",main=NA) 
     xfit<-seq(min(x),max(x),length=40) 
     yfit<-dnorm(xfit,mean=mean(x),sd=sd(x)) 
     yfit <- yfit*diff(h$mids[1:2])*length(x) 
-    lines(xfit, yfit, col="blue", lwd=2)
+    lines(xfit, yfit, col="red", lwd=2)
+    
+
+    
     
     
     
--- ui.R
+++ ui.R
@@ -2,15 +2,17 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel(title=h4("Distribution of HDB price around rulang primary",align="center")),
+  titlePanel(title=h4("Distribution of HDB price around rulang primary school",align="center")),
   sidebarLayout(
     sidebarPanel(
-      selectInput("distance", "1. Select the type of distnace", choices=c("total"=10,"within 1km"=1,"within2km"=2)),
+      selectInput("var", "1. Select the type of price", choices=c("price"=1,"real price"=2,"log realprice"=3)),
       br(),
-      sliderInput("year","2.Select year for the histogram", min=1990, max=2018, value=1990,animate=TRUE),
+      selectInput("distance", "2. Select the type of distnace", choices=c("total"=10,"within 1km"=1,"within2km"=2)),
+      br(),
+      sliderInput("year","3.Select year for the histogram", min=1990, max=2018, value=1990,animate=TRUE),
       br(),
       
-      sliderInput("bins","2.Select the number of BINs for the histogram", min=10, max=30, value=20)
+      sliderInput("bins","4.Select the number of BINs for the histogram", min=10, max=30, value=20)
     ),
     
     mainPanel(
